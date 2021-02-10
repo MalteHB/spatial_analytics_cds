accd55c310d295631cd974db8fb9925029ce469c
hjb <hjb@kmd.dk>
2021-02-10 17:27:14 +0100

    made Rproj

--- Week02/HW/exercise_3.R
+++ Week02/HW/exercise_3.R
@@ -36,25 +36,40 @@ library(spDataLarge)
 hist(nz$Population)
 
 # This line of code applies the 'pretty' style rounding legend numbers. Try different numbers of classes.
-tm_shape(nz) + tm_polygons(col = "Population", style = "pretty", n = 4)
+pretty <- tm_shape(nz) + tm_polygons(col = "Population", style = "pretty", n = 4)
 
 # "Jenks" style further smooths over the gaps
-tm_shape(nz) + tm_polygons(col = "Population", style = "jenks", n = 5)
+jenks <- tm_shape(nz) + tm_polygons(col = "Population", style = "jenks", n = 5)
 
 # quantile style divides into 5 even groups
-tm_shape(nz) + tm_polygons(col = "Population", style = "quantile", n=5)
+quantile <- tm_shape(nz) + tm_polygons(col = "Population", style = "quantile", n=5)
 
 # Equal interval style divides the distribution into even groups
-tm_shape(nz) + tm_polygons(col = "Population", style = "equal", n = 5)
+equal <- tm_shape(nz) + tm_polygons(col = "Population", style = "equal", n = 5)
 
 # Write maps above to objects and plot them side by side 
 # with tmap_arrange() for better comparison
-tmap_arrange(___,___,___,____)
+tmap_arrange(pretty,jenks,quantile,equal)
 
 
 #### Exercise I ####
 
 # 1. What are the advantages and disadvantages of each classification method?
+pretty
+# Pretty: A clear disadvantage here is that it does not manage to show the granularity of the population of New Zealand.
+# An advantage is that the different legend divisions are easy to distinguish, and is linear.
+
+jenks
+# Jenks: Manages to show the granularity of the population as it nicely shows the distribution across New Zealand.
+# However, as the legend is not linear, it difficult to assess the different location-percentages of the population.
+
+quantile
+# Quantile: Again - high granularity, but not as much as 'jenks'. It rates a lot of the areas as highly populated.
+# Does however, manage to give a better view at the distribution than the 'pretty' style.
+
+equal
+# Equal:
+
 # 2. Choose the best classification and create a map with easily legible legend and all other essentials.
 # (Select a suitable color palette from http://colorbrewer2.org/, north arrow, scale, map title, 
 # legend title, reasonable number of breaks in the classification )
@@ -66,6 +81,7 @@ tmap_arrange(___,___,___,____)
 # Your solution
 
 # /Start Code/ #
+
 
 
 # /End Code/ #
